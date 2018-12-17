3a081cfabf07d01322e3fbfabb33574ce50659a8
sun <1760167815@qq.com>
2018-12-17 14:24:18 +0800

    /

--- src/test/java/ss/HelloApplicationTests.java
+++ src/test/java/ss/HelloApplicationTests.java
@@ -1,9 +1,8 @@
-package com.neo;
+package ss;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
